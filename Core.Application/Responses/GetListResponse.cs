3615804c4741f265a2b84a4066893bba3e38b355
muhsinsaral <muhsinsrl@gmail.com>
2024-01-02 08:54:33 +0300

    Proje dosyası ekle.

---
+++ Core.Application/Responses/GetListResponse.cs
@@ -0,0 +1,18 @@
+﻿using Core.Persistence.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Responses;
+
+public class GetListResponse<T> : BasePageableModel
+{
+    private IList<T> _items;
+    public IList<T> Items
+    {
+        get => _items ??= Array.Empty<T>();
+        set => _items = value;
+    }
+}
