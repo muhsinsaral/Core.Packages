5d01d88d4f2b455575548300ffd676fbfc9174f2
muhsinsaral <muhsinsrl@gmail.com>
2024-01-02 18:16:55 +0300

    Project building

---
+++ Core.Application/PipeLines/Validation/RequestValidationBehavior.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.PipeLines.Validation;
+
+public class RequestValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        ValidationContext<object> context = new(request);
+        
+        IEnumerable<ValidationException> errors = 
+    }
+}
