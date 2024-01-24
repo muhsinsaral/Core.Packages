202dfc428944f85b3b74f125474684772fc07566
muhsinsaral <muhsinsrl@gmail.com>
2024-01-24 16:18:30 +0300

    PipeLine Implementation

--- Core.Application/PipeLines/Validation/RequestValidationBehavior.cs
+++ Core.Application/PipeLines/Validation/RequestValidationBehavior.cs
@@ -1,20 +1,37 @@
-﻿using FluentValidation;
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+using FluentValidation;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ValidationException = Core.CrossCuttingConcerns.Exceptions.Types.ValidationException;
 
 namespace Core.Application.PipeLines.Validation;
 
 public class RequestValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
-    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public RequestValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         ValidationContext<object> context = new(request);
-        
-        IEnumerable<ValidationException> errors = 
+
+        IEnumerable<ValidationExceptionModel> errors = _validators
+            .Select(v => v.Validate(context))
+            .SelectMany(result => result.Errors)
+            .Where(error => error != null)
+            .GroupBy(
+            keySelector: error => error.PropertyName,
+            resultSelector: (propertyName, errors) =>
+            new ValidationExceptionModel { Property = propertyName, Errors = errors.Select(error => error.ErrorMessage) })
+            .ToList();
+        if (errors.Any())
+            throw new ValidationException(errors);
+
+        TResponse response = await next();
+        return response;
     }
 }
